ca4995b7c81ff5af1fd9578d7dbe8cffbbfb9376
ykm989 <ykm989@naver.com>
2023-04-06 18:30:31 +0900

    BRAIN-7 🤒 First Commit

---
+++ Brainy/Brainy/BrainyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainyApp.swift
+//  Brainy
+//
+//  Created by 김경호 on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct BrainyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
