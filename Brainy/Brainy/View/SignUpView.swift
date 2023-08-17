5583448b4d73f1a035b133a9709d49c6ce82a828
ykm989 <ykm989@naver.com>
2023-08-17 17:06:15 +0900

    BRAIN-10 💄 STYLE : UI Change

--- Brainy/Brainy/View/SignUpView.swift
+++ Brainy/Brainy/View/SignUpView.swift
@@ -18,46 +18,77 @@ struct SignUpView: View {
     
     var body: some View {
         VStack(alignment: .center){
-            Form{
-                Section(header: Text("Email")) {
-                    TextField("Email", text: $email)
-                        .keyboardType(.emailAddress)
-                        .textInputAutocapitalization(.never)
-
-                }
-                
-                Section(header: Text("Password")) {
-                    SecureField("New Password", text: $pw)
-                        .textInputAutocapitalization(.never)
-
-//                    SecureField("Confirm Password", text: $pwCheck)
-//                        .textInputAutocapitalization(.never)
-                }
-                
-                Section(header: Text("Name")) {
-                    TextField("name", text: $nickname)
-                        .textInputAutocapitalization(.never)
-                }
-                
-                Button {
-                    viewModel.SignUp(email, pw)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        print("Dispatch")
-                        signupSuccess = viewModel.loginSuccess
-                        signupFail = !viewModel.loginSuccess
+            Spacer()
+            
+            Text("Sign Up")
+                .font(.custom("Bangers-Regular", size: 40))
+                .foregroundColor(Color("MainColor"))
+                .padding(.bottom, 50)
+            VStack {
+                Text("Email")
+                    .frame(width: 300, alignment: .leading)
+                TextField(" Input your Email", text: $email)
+                    .frame(width: 300, height: 30)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 2)
+                            .stroke(Color.gray)
                     }
-                } label: {
-                    Text("Sign Up")
-                }.alert("SignUp Fail", isPresented: $signupFail, actions: {
-                    Button("OK", role: .cancel){}
-                })
-                .fullScreenCover(isPresented: $signupSuccess) {
-                    MainView()
-                }
-//                .sheet(isPresented: $signupSuccess, content: {
-//                    MainView()
-//                })
+            }.padding()
+            
+            VStack{
+                Text("Password")
+                    .frame(width: 300, alignment: .leading)
+                TextField(" Input your Password", text: $pw)
+                    .frame(width: 300, height: 30)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 2)
+                            .stroke(Color.gray)
+                    }
             }
+            Spacer()
+            Spacer()
+            
+            
+//            Form{
+//                Section(header: Text("Email")) {
+//                    TextField("Email", text: $email)
+//                        .keyboardType(.emailAddress)
+//                        .textInputAutocapitalization(.never)
+//
+//                }
+//
+//                Section(header: Text("Password")) {
+//                    SecureField("New Password", text: $pw)
+//                        .textInputAutocapitalization(.never)
+//
+////                    SecureField("Confirm Password", text: $pwCheck)
+////                        .textInputAutocapitalization(.never)
+//                }
+//
+//                Section(header: Text("Name")) {
+//                    TextField("name", text: $nickname)
+//                        .textInputAutocapitalization(.never)
+//                }
+//
+//                Button {
+//                    viewModel.SignUp(email, pw)
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                        print("Dispatch")
+//                        signupSuccess = viewModel.loginSuccess
+//                        signupFail = !viewModel.loginSuccess
+//                    }
+//                } label: {
+//                    Text("Sign Up")
+//                }.alert("SignUp Fail", isPresented: $signupFail, actions: {
+//                    Button("OK", role: .cancel){}
+//                })
+//                .fullScreenCover(isPresented: $signupSuccess) {
+//                    MainView()
+//                }
+////                .sheet(isPresented: $signupSuccess, content: {
+////                    MainView()
+////                })
+//            }
         }
         
     }
